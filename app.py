601443dca7fdabe6f54e15a19b6ab99cc3b3abe8
progettazionemauro <progettazione@studiogb.org>
2023-07-30 19:43:42 +0200

    flusso di autorizzazione funzionate

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
 from flask_socketio import SocketIO, emit
 from dotenv import load_dotenv
 import sqlite3
 import os
 import ssl
 from werkzeug.utils import secure_filename
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'  # Replace with your own secret key
+
+# Initialize the Flask-Login extension
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Redirect unauthorized users to the login page
 
 # Define the path where the files will be stored on the server
 UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')  # Using 'uploads' folder inside the app's root path
@@ -20,6 +25,29 @@ DATABASE = 'database.db'
 # Load environment variables from .env file
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv('.env')
+
+# Create a User Model
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+# Replace this with your own user authentication logic
+def authenticate_user(username, password):
+    # Add your user authentication logic here
+    # For demonstration purposes, we'll use a dummy username and password
+    if username == 'demo_user' and password == 'demo_password':
+        return User(user_id=1)  # For simplicity, we assume a single user with ID 1
+    return None
+
+# User loader function for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    # Replace this with your code to load a user from the database or another data source
+    # For this example, we'll assume there's a dictionary of users where the user ID is the key
+    users = {
+        1: User(1)  # Dummy user with ID 1 for demonstration
+    }
+    return users.get(int(user_id))
 
 def create_table():
     conn = sqlite3.connect(DATABASE)
@@ -38,6 +66,7 @@ def index():
     return render_template('index.html', objects=objects)
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add_object():
     name = request.form['name']
     filename = None  # Initialize filename to None
@@ -176,6 +205,25 @@ def object_details_page():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = authenticate_user(username, password)
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 # Utilizzo nel caso generale
 if __name__ == '__main__':
